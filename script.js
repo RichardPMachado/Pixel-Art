2a81e58e0f442cd9c96d3afe585a1b4ae2b85016
Richard P Machado <richardmachado2211@gmail.com>
2022-07-08 04:05:06 -0300

    Todos requisitos finalizados

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
 const numBoxColor = 4;
-const size = 5;
-const numBox = size * size;
+
 // ================ Criar caixas e suas repectivas cores ================================
+function randomColor() {
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+  return `rgb(${red},${green},${blue})`;
+}
 
 const color = ['firstBox', 'secondBox', 'thirdBox', 'fourthBox', 'fifthBox', 'sixthBox'];
-let colorLi = [];
+
 function createBoxColors(number) {
+  let colorLi = [];
   const paletteUl = document.getElementById('color-palette');
   for (let index = 0; index < number; index += 1) {
     colorLi = document.createElement('li');
     paletteUl.appendChild(colorLi);
     colorLi.className = 'color';
     colorLi.classList.add(color[index]);
+    colorLi.style.backgroundColor = randomColor();
     // ========================== Create Class Selected ===================================
     if (color[index] === 'firstBox') {
+      colorLi.style.backgroundColor = 'black';
       colorLi.classList.add('selected');
     }
   }
 }
-
 createBoxColors(numBoxColor);
 
 // ========================== Create Pixel Board ===================================
-
 const pixelBoard = document.getElementById('pixel-board');
-function createPixelsBoard(number) {
+function createPixelsBoard(size) {
+  const numBox = size * size;
   pixelBoard.style.setProperty('--size', size);
-  for (let index = 0; index < number; index += 1) {
+  for (let index = 0; index < numBox; index += 1) {
     const pixel = document.createElement('div');
     pixelBoard.appendChild(pixel);
     pixel.className = 'pixel';
   }
 }
-createPixelsBoard(numBox);
-
+createPixelsBoard(5);
 // ========================== Check Class Selected ===================================
 const colorsLi = document.querySelectorAll('.color');
 function selectedColor(event) {
@@ -50,26 +56,54 @@ for (let index = 0; index < numBoxColor; index += 1) {
 }
 
 // ========================== Add Color Class Selected ===================================
-const pixel = document.querySelectorAll('.pixel');
 function addColor(event) {
   for (let index = 0; index < 4; index += 1) {
     if (colorsLi[index].classList.contains('selected')) {
-      event.target.classList.toggle(color[index]);
+      const clickAlvo = event.target;
+      clickAlvo.style.backgroundColor = colorsLi[index].style.backgroundColor;
     }
   }
 }
-
-for (let index = 0; index < numBox; index += 1) {
+const pixel = document.querySelectorAll('.pixel');
+for (let index = 0; index < pixel.length; index += 1) {
   pixel[index].addEventListener('click', addColor);
 }
 
 // ========================== Clear Board ===================================
-const clearBtn = document.getElementById('clear-board');
-const pixelClear = document.querySelectorAll('.pixel');
-
+const clear = document.querySelector('#clear-board');
 function clearBoard() {
-  for (let index = 0; index < numBox; index += 1) {
-    pixelClear[index].className = 'pixel';
+  const box = document.querySelectorAll('.pixel');
+  for (let index = 0; index < box.length; index += 1) {
+    box[index].style.background = 'white';
   }
 }
-clearBtn.addEventListener('click', clearBoard);
+clear.addEventListener('click', clearBoard);
+
+// ========================== Min & Max Size =========================================================
+
+function sizeMinMax() {
+  const boxQtd = document.getElementById('board-size');
+  if (boxQtd.value < 5) {
+    boxQtd.value = 5;
+  }
+  if (boxQtd.value > 50) {
+    boxQtd.value = 50;
+  }
+}
+
+function inputVazio() {
+  const input = document.getElementById('board-size');
+  if (input.value === '') {
+    alert('Board inválido!');
+  } else {
+    const box = document.querySelectorAll('.pixel');
+    for (let index = 0; index < box.length; index += 1) {
+      box[index].remove();
+    }
+    sizeMinMax();
+    createPixelsBoard(input.value);
+  }
+}
+
+const clickSize = document.getElementById('generate-board');
+clickSize.addEventListener('click', inputVazio);
